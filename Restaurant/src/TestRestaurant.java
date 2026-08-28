00b6265f394b38d9048a6defdfb294a9ed720af3
ThanhDuy <thanhduy19042020@gmail.com>
2026-08-28 14:42:18 +0700

    Delete Restaurant directory

--- Restaurant/src/TestRestaurant.java
+++
@@ -1,7 +0,0 @@
-import junit.framework.TestCase;
-
-public class TestRestaurant extends TestCase {
-    public void testConstructor(){
-        new Restaurant("Chè Thanh Duy","Chè Đậu Đỏ","cheap",new Intersection("40th", "Hang Tre"));
-    }
-}
